cf56b39c758b7ee9f726b8738b32ab0fd9e143e1
Oscariolo <oscariolopinop21@gmail.com>
2025-05-12 22:34:08 -0500

    Created base project and docker-compose.yaml for kafka

---
+++ distributednewsserver/src/main/java/com/springbootserver/distributednewsserver/DistributednewsserverApplication.java
@@ -0,0 +1,13 @@
+package com.springbootserver.distributednewsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributednewsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributednewsserverApplication.class, args);
+	}
+
+}
---
+++ distributednewsserver/src/test/java/com/springbootserver/distributednewsserver/DistributednewsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootserver.distributednewsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributednewsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
